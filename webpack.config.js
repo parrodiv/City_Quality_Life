7a22ef7b5e8ad0af1739352fa738d395773eb4bf
Alessandro Parrilla <paggio96@gmail.com>
2022-03-22 22:32:19 +0100

    -creato webpack config, ed impostato la modalità su development momentaneamente, ora riesco a vedere il codice non minimizzato

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const { dirname } = require("path");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    "devtool": false,
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    }
+}
