847d63f95afc2ac4945254b9dc62244625ef370e
Alessandro Parrilla <paggio96@gmail.com>
2022-03-23 21:41:24 +0100

    INSERITO IL MERGE TRA I FILE DI CONFIGURAZIONE WEBPACK
    -`npm start` avvierà webpack in modalita development
    -`npm run build` avvierà in modalità production e pertanto il codice sarà minimizzato

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,12 @@
-
 const path = require("path");
+const common = require("./webpack.common");
+const {merge} = require("webpack-merge"); 
 
-module.exports = {
+module.exports = merge(common, {
     mode: "development",
     "devtool": false,
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
     },
-}
+})
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,8 @@
-
 const path = require("path");
+const common = require("./webpack.common");
+const {merge} = require("webpack-merge"); 
 
-module.exports = {
+module.exports = merge(common, {
     mode: "production",
     "devtool": false,
     output: {
@@ -9,4 +10,4 @@ module.exports = {
         path: path.resolve(__dirname, "dist")
     },
     
-}
+})
