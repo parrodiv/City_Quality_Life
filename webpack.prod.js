8400b55e19c97a000b926741f1e6b4c3f20bc0cd
Alessandro Parrilla <paggio96@gmail.com>
2022-03-24 15:39:46 +0100

    modificato webpack.prod per il non far sovrascrivere la minimizzazione dal minimizer css

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const {merge} = require("webpack-merge");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
@@ -15,7 +16,8 @@ module.exports = merge(common, {
     },
     optimization: {
         minimizer: [
-          new CssMinimizerPlugin(),
+          new CssMinimizerPlugin(), new TerserPlugin()
+          //terser plugin minimizza javascript, ma il css minimizer ha sovrascritto il minimizer javascript, pertanto lo inserisco dopo il cssMinimizer
         ],
     },
     plugins: [
